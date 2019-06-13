f8acdae17e7719a931a6716611106e1f18a7d011
Pablo Escriva <pabloescrivagallardo@gmail.com>
2019-06-13 12:50:21 +0200

    added delete requests

--- db/media.js
+++ db/media.js
@@ -1,13 +1,19 @@
 const db = require('./index');
 const pgp = require('pg-promise')();
 
-//Read
-exports.getMedias = () => db.any('SELECT * FROM media')
-exports.getMediaContents = () => db.any('SELECT * FROM media_content')
-exports.getMediaTypes = () => db.any('SELECT * FROM media_types')
 //Create
 exports.addMedia = (media) => db.none('INSERT INTO media ($1:name) VALUES ($1:list)',[media])
 exports.addMediaType = (type) => db.none('INSERT INTO media_type (type) VALUES ($1)',[type])
 exports.addMediaContent = (content) => db.none('INSERT INTO media_content (content) VALUES ($1)',[content])
+//Read
+exports.getMedias = () => db.any('SELECT * FROM media')
+exports.getMediaContents = () => db.any('SELECT * FROM media_content')
+exports.getMediaTypes = () => db.any('SELECT * FROM media_type')
+//Delete
+exports.deleteMedia = (media_id) => db.none('DELETE FROM media WHERE id=($1)',[media_id])
+exports.deleteMediaContent = (media_Content_id) => db.none('DELETE FROM media_content WHERE id=($1)',[media_Content_id])
+exports.deleteMediaType = (media_Type_id) => db.none('DELETE FROM media_type WHERE id=($1)',[media_Type_id])
+
+
 
 
