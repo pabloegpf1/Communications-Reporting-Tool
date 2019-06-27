ef34326932b34a8cd7d5f093d2793bef7b33345c
Pablo Escriva <pabloescrivagallardo@gmail.com>
2019-06-27 09:23:35 +0200

    Added favicon

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/views/public')));
+app.use('/favicon.ico', express.static('images/favicon.ico'));
 app.use(session({
   secret: 'secret',
   saveUninitialized: true,
