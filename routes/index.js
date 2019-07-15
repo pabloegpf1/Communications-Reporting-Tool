19d94b792559497fa01cb740705d7db790b7177b
Pablo Escriva <pabloescrivagallardo@gmail.com>
2019-07-15 08:58:01 +0200

    Changed Navbar

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var Document = require('../controllers/document')
 
 router.get('/', function(request,response) {
     response.render('login',{
         authMessage: request.flash('authMessage')
     })
+});
+
+router.get('/pdf', function(request,response) {
+    Document.createPDF(request,response)
+});
+
+router.post('/pdf', function(request,response) {
+    Document.downloadPDF(request,response)
 });
 
 module.exports = router;
