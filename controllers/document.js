a68e2186b913284f7fd5f555bdb00764074e91d3
Pablo Escriva <pabloescrivagallardo@gmail.com>
2019-08-01 12:28:48 +0200

    Initial iteration of PDF generation

--- controllers/document.js
+++ controllers/document.js
@@ -1,17 +1,48 @@
+var Impact = require("../models/queries/impact");
+var Dissemination = require("../models/queries/dissemination");
+var SMshare = require("../models/queries/smShare");
+
 var fs = require('fs');
 var pdf = require('html-pdf');
 var options = { format: 'Letter' };
 var ejs = require('ejs');
-var Impact = require("../models/queries/impact");
+
+options = {
+ 
+  "format": "A4",        // allowed units: A3, A4, A5, Legal, Letter, Tabloid
+  "orientation": "portrait", // portrait or landscape
+ 
+  "border": {
+    "top": "2.54cm",            // default is 0, units: mm, cm, in, px
+    "right": "2.54cm",
+    "bottom": "2.54cm",
+    "left": "2.54cm"
+  },
+
+  "type": "pdf",             // allowed file types: png, jpeg, pdf
+ 
+}
 
 exports.createPrImpactReport = (request, response) => {
-  Impact.getImpactById(1) //TODO
-  .then(impact => {
-    ejs.renderFile('./views/templates/PrImpactReport.ejs', { Impact: impact }, function(err, result) {
-      pdf.create(result, options).toFile('./documents/PrImpactReport.pdf', function(err,res) {
-        if (err) return console.log(err);
-        response.download('./documents/PrImpactReport.pdf');
-      });
-    });
+  Dissemination.getDisseminationsByDate(request.body.initial_date,request.body.final_date)
+  .then(disseminations => {
+    Impact.getImpactsByDate(request.body.initial_date,request.body.final_date)
+    .then(impacts => {
+      SMshare.getSharesByDate(request.body.initial_date,request.body.final_date)
+      .then(shares => {
+        ejs.renderFile('./views/templates/PrImpactReport.ejs', { 
+          disseminations: disseminations,
+          impacts: impacts,
+          shares: shares,
+          initialDate:request.body.initial_date,
+          finalDate: request.body.final_date
+        }, function(err, result) {
+          pdf.create(result, options).toFile('./documents/PrImpactReport.pdf', function(err,res) {
+            if (err) return console.log(err);
+            response.download('./documents/PrImpactReport.pdf');
+          })
+        })
+      })
+    })
   })
 };
