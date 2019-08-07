b5a294f5e102b9560a2c9fd0543a39728687d68a
Pablo Escriva <pabloescrivagallardo@gmail.com>
2019-08-07 12:10:37 +0200

    Added SM Shares worksheet

--- controllers/spreadsheet.js
+++ controllers/spreadsheet.js
@@ -1,11 +1,24 @@
 var Excel = require('exceljs');
 var DB = require("../models/queries/spreadsheet");
 
-exports.createPressClippingSpreadsheet = (request, response) => {
+exports.createPressClippingSpreadsheet = async (request, response) => {
   var workbook = new Excel.Workbook();
+  await createClippingWorksheet(workbook,request.body.initial_date,request.body.final_date)
+  await createSMShareWorksheet(workbook,request.body.initial_date,request.body.final_date)
+  saveAndDownload(workbook,response)
+};
+
+function saveAndDownload(workbook,response){
+  workbook.xlsx.writeFile('./documents/PRESS_RELEASE_CLIPPING.xlsx').then(function() {
+    response.download('./documents/PRESS_RELEASE_CLIPPING.xlsx');
+  });
+}
+
+async function createClippingWorksheet(workbook,initial_date,final_date){
   var worksheet = workbook.addWorksheet('Clipping');
-  DB.getImpacts().then((impacts) => {
-    var headers = ["Audience"];
+  await DB.getImpacts(initial_date,final_date).then((impacts) => {
+    if(impacts.length == 0) return 
+    var headers = ["Audience","Month"];
     Object.keys(impacts[0]).forEach(key => {
       headers.push(key);
     })
@@ -16,9 +29,12 @@ exports.createPressClippingSpreadsheet = (request, response) => {
       fgColor:{argb:'98fb98'},
       bgColor:{argb:'FF0000FF'}
     };
-    for(let i  in impacts){
+    for(let i in impacts){
+      const date = new Date(impacts[i].Date)
       var cols = [];
       cols.push(1);
+      cols.push(date.getMonth()+1);
+      impacts[i].Date = new Date( Date.UTC( date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()))
       Object.keys(impacts[i]).forEach(key => {
         cols.push(impacts[i][key]);
       })
@@ -28,9 +44,73 @@ exports.createPressClippingSpreadsheet = (request, response) => {
       from: 'A1',
       to: 'V1',
     }
-    workbook.xlsx.writeFile('./documents/PRESS_RELEASE_CLIPPING.xlsx').then(function() {
-      response.download('./documents/PRESS_RELEASE_CLIPPING.xlsx');
-    });
   })
-};
+}
 
+async function createSMShareWorksheet(workbook,initial_date,final_date){
+  var worksheet = workbook.addWorksheet('SM Shares');
+  await DB.getShares(initial_date,final_date).then((shares) => {
+    console.log(shares)
+    var headers = ["Number of Posts","Month"];
+    if(shares.length == 0) return 
+    Object.keys(shares[0]).forEach(key => {
+      headers.push(key);
+    })
+    let headerRow = worksheet.addRow(headers);
+    headerRow.fill = {
+      type: 'pattern',
+      pattern:'solid',
+      fgColor:{argb:'98fb98'},
+      bgColor:{argb:'FF0000FF'}
+    };
+    for(let i in shares){
+      const date = new Date(shares[i].Date)
+      var cols = [];
+      cols.push(1);
+      cols.push(date.getMonth()+1);
+      shares[i].Date = new Date( Date.UTC( date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()))
+      Object.keys(shares[i]).forEach(key => {
+        cols.push(shares[i][key]);
+      })
+      worksheet.addRow(cols);
+    }
+    worksheet.autoFilter = {
+      from: 'A1',
+      to: 'V1',
+    }
+  })
+}
+
+async function createSMShareWorksheet(workbook,initial_date,final_date){
+  var worksheet = workbook.addWorksheet('SM Shares');
+  await DB.getShares(initial_date,final_date).then((shares) => {
+    console.log(shares)
+    var headers = ["Number of Posts","Month"];
+    if(shares.length == 0) return 
+    Object.keys(shares[0]).forEach(key => {
+      headers.push(key);
+    })
+    let headerRow = worksheet.addRow(headers);
+    headerRow.fill = {
+      type: 'pattern',
+      pattern:'solid',
+      fgColor:{argb:'98fb98'},
+      bgColor:{argb:'FF0000FF'}
+    };
+    for(let i in shares){
+      const date = new Date(shares[i].Date)
+      var cols = [];
+      cols.push(1);
+      cols.push(date.getMonth()+1);
+      shares[i].Date = new Date( Date.UTC( date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()))
+      Object.keys(shares[i]).forEach(key => {
+        cols.push(shares[i][key]);
+      })
+      worksheet.addRow(cols);
+    }
+    worksheet.autoFilter = {
+      from: 'A1',
+      to: 'M1',
+    }
+  })
+}
--- models/queries/spreadsheet.js
+++ models/queries/spreadsheet.js
@@ -1,11 +1,18 @@
 const pgp = require("pg-promise")();
 const db = require("../config/db-connection");
 
-let baseQuery = `i.date as "Date",m.name as "Media Name",m.url as "Media Web", m_t.type as "Type of Media",m.content as "Media Content", m.coverage as "Media Coverage", 
+let impactQuery = `i.date as "Date",m.name as "Media Name",m.url as "Media Web", m_t.type as "Type of Media",m.content as "Media Content", m.coverage as "Media Coverage", 
                 i.media_section as "Section", i.headline as "Headline",m.format as "Format", i_type.type as "Type of Publication", i.has_video as "Video", 
                 (i.photo_count > 0) as "Photos", i.photo_count as "Nº Photos", i.statements as "Statements", i.spokesperson as "Spokesperson",
                 i.language as "Language", i.proactivity as "Proactivity", i.uploaded as "Uploaded", d.pr_news as "PR/News", d.summary as "Summary", i.source_url as "Original URL"
                 FROM impact i, impact_type i_type, media m, media_type m_t, dissemination d, classification c
                 WHERE m.type = m_t.id AND i.media = m.id AND i.type = i_type.id AND i.dissemination = d.id AND i.classification = c.id`;
 
-exports.getImpacts = () =>db.any(`SELECT $1:raw ORDER BY i.date DESC`, [baseQuery]);
+let shareQuery = `sm_s.date as "Date", s_media.social_media as "Media Name", s_media_a.name as "Account Name", sm_s.message as "Message", sm_s.language as "Language",
+                sm_s.proactivity as "Proactivity", sm_s.uploaded as "Uploaded", d.pr_news as "PR/News",c.classification as "Classification", d.summary as "Summary",
+                sm_s.source_url as "Source URL", sm_s.comments as "Comments"
+                FROM sm_share sm_s, social_media s_media, social_media_account s_media_a, dissemination d, classification c 
+                WHERE sm_s.social_media = s_media.id AND sm_s.account = s_media_a.id AND sm_s.dissemination = d.id AND sm_s.classification = c.id`;
+
+exports.getImpacts = (initial_date,final_date) =>db.any(`SELECT $1:raw AND i.date >= $2 AND i.date <= $3 ORDER BY i.date`, [impactQuery,initial_date,final_date]);
+exports.getShares = (initial_date,final_date) =>db.any(`SELECT $1:raw AND sm_s.date >= $2 AND sm_s.date <= $3 ORDER BY sm_s.date`, [shareQuery,initial_date,final_date]);
