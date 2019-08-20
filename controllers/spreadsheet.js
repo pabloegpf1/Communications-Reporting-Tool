10dd2c54178bad0ecb741e8aa7786723797b3c94
Pablo Escriva <pabloescrivagallardo@gmail.com>
2019-08-20 12:06:08 +0200

    Added Dissemination sheet to excel

--- controllers/spreadsheet.js
+++ controllers/spreadsheet.js
@@ -5,6 +5,7 @@ exports.createPressClippingSpreadsheet = async (request, response) => {
   var workbook = new Excel.Workbook();
   await createClippingWorksheet(workbook,request.body.initial_date,request.body.final_date)
   await createSMShareWorksheet(workbook,request.body.initial_date,request.body.final_date)
+  await createDisseminationWorksheet(workbook,request.body.initial_date,request.body.final_date)
   saveAndDownload(workbook,response)
 };
 
@@ -18,7 +19,7 @@ async function createClippingWorksheet(workbook,initial_date,final_date){
   var worksheet = workbook.addWorksheet('Clipping');
   await DB.getImpacts(initial_date,final_date).then((impacts) => {
     if(impacts.length == 0) return 
-    var headers = ["Audience","Month"];
+    var headers = ["Month"];
     Object.keys(impacts[0]).forEach(key => {
       headers.push(key);
     })
@@ -32,7 +33,6 @@ async function createClippingWorksheet(workbook,initial_date,final_date){
     for(let i in impacts){
       const date = new Date(impacts[i].Date)
       var cols = [];
-      cols.push(1);
       cols.push(date.getMonth()+1);
       impacts[i].Date = new Date( Date.UTC( date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()))
       Object.keys(impacts[i]).forEach(key => {
@@ -50,7 +50,6 @@ async function createClippingWorksheet(workbook,initial_date,final_date){
 async function createSMShareWorksheet(workbook,initial_date,final_date){
   var worksheet = workbook.addWorksheet('SM Shares');
   await DB.getShares(initial_date,final_date).then((shares) => {
-    console.log(shares)
     var headers = ["Number of Posts","Month"];
     if(shares.length == 0) return 
     Object.keys(shares[0]).forEach(key => {
@@ -81,10 +80,9 @@ async function createSMShareWorksheet(workbook,initial_date,final_date){
   })
 }
 
-async function createSMShareWorksheet(workbook,initial_date,final_date){
+async function createDisseminationWorksheet(workbook,initial_date,final_date){
   var worksheet = workbook.addWorksheet('SM Shares');
-  await DB.getShares(initial_date,final_date).then((shares) => {
-    console.log(shares)
+  await DB.getDisseminations(initial_date,final_date).then((shares) => {
     var headers = ["Number of Posts","Month"];
     if(shares.length == 0) return 
     Object.keys(shares[0]).forEach(key => {
@@ -111,6 +109,38 @@ async function createSMShareWorksheet(workbook,initial_date,final_date){
     worksheet.autoFilter = {
       from: 'A1',
       to: 'M1',
+    }
+  })
+}
+
+async function createDisseminationWorksheet(workbook,initial_date,final_date){
+  var worksheet = workbook.addWorksheet('Dissemination Data');
+  await DB.getDisseminations(initial_date,final_date).then((disseminations) => {
+    if(disseminations.length == 0) return 
+    var headers = ["Month"];
+    Object.keys(disseminations[0]).forEach(key => {
+      headers.push(key);
+    })
+    let headerRow = worksheet.addRow(headers);
+    headerRow.fill = {
+      type: 'pattern',
+      pattern:'solid',
+      fgColor:{argb:'98fb98'},
+      bgColor:{argb:'FF0000FF'}
+    };
+    for(let i in disseminations){
+      const date = new Date(disseminations[i].Date)
+      var cols = [];
+      cols.push(date.getMonth()+1);
+      disseminations[i].Date = new Date( Date.UTC( date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()))
+      Object.keys(disseminations[i]).forEach(key => {
+        cols.push(disseminations[i][key]);
+      })
+      worksheet.addRow(cols);
+    }
+    worksheet.autoFilter = {
+      from: 'A1',
+      to: 'H1',
     }
   })
 }
--- models/queries/spreadsheet.js
+++ models/queries/spreadsheet.js
@@ -4,7 +4,7 @@ const db = require("../config/db-connection");
 let impactQuery = `i.date as "Date",m.name as "Media Name",m.url as "Media Web", m_t.type as "Type of Media",m.content as "Media Content", m.coverage as "Media Coverage", 
                 i.media_section as "Section", i.headline as "Headline",m.format as "Format", i_type.type as "Type of Publication", i.has_video as "Video", 
                 (i.photo_count > 0) as "Photos", i.photo_count as "Nº Photos", i.statements as "Statements", i.spokesperson as "Spokesperson",
-                i.language as "Language", i.proactivity as "Proactivity", i.uploaded as "Uploaded", d.pr_news as "PR/News", d.summary as "Summary", i.source_url as "Original URL"
+                i.language as "Language", i.proactivity as "Proactivity", i.uploaded as "Uploaded", d.pr_news as "PR/News", d.summary as "Summary", i.source_url as "Original URL", NOT d.include_in_report as "Other Media Impact"
                 FROM impact i, impact_type i_type, media m, media_type m_t, dissemination d, classification c
                 WHERE m.type = m_t.id AND i.media = m.id AND i.type = i_type.id AND i.dissemination = d.id AND i.classification = c.id`;
 
@@ -14,5 +14,10 @@ let shareQuery = `sm_s.date as "Date", s_media.social_media as "Media Name", s_m
                 FROM sm_share sm_s, social_media s_media, social_media_account s_media_a, dissemination d, classification c 
                 WHERE sm_s.social_media = s_media.id AND sm_s.account = s_media_a.id AND sm_s.dissemination = d.id AND sm_s.classification = c.id`;
 
+
+let disseminationQuery = `date as "Date", headline as "Headline", summary as "Summary", lead_paragraph as "Lead Paragraph", pr_news as "PR/NEWS", url as "URL",
+                          NOT include_in_report as "Other Media Dissemination" FROM dissemination`;
+
 exports.getImpacts = (initial_date,final_date) =>db.any(`SELECT $1:raw AND i.date >= $2 AND i.date <= $3 ORDER BY i.date`, [impactQuery,initial_date,final_date]);
 exports.getShares = (initial_date,final_date) =>db.any(`SELECT $1:raw AND sm_s.date >= $2 AND sm_s.date <= $3 ORDER BY sm_s.date`, [shareQuery,initial_date,final_date]);
+exports.getDisseminations = (initial_date,final_date) =>db.any(pgp.as.format(`SELECT $1:raw WHERE date >= $2 AND date <= $3 ORDER BY date`,[disseminationQuery,initial_date,final_date]));
